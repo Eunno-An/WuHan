c1da9d16107128c6838134378c56f2a6a8c1ef55
Eunno An <12171647@inha.edu>
2020-01-31 21:23:29 +0900

    commit eunno

--- app/src/main/java/com/example/wuhan/MainActivity.java
+++ app/src/main/java/com/example/wuhan/MainActivity.java
@@ -25,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
 
     //eunno init
     private AppBarConfiguration mAppBarConfiguration;
+    //crawl commit
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
