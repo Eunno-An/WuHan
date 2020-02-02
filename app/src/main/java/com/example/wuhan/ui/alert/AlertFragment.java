fe4ef676af89cf9aaaf028606f7dfcb25acf9002
Ina-Son <33450465+Ina-Son@users.noreply.github.com>
2020-02-03 04:02:31 +0900

    디비 연동2 - 확진자 등
    alert 페이지에서 쓸 정보 불러옴

--- app/src/main/java/com/example/wuhan/ui/alert/AlertFragment.java
+++ app/src/main/java/com/example/wuhan/ui/alert/AlertFragment.java
@@ -1,6 +1,7 @@
 package com.example.wuhan.ui.alert;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,8 +9,15 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.wuhan.R;
+import com.example.wuhan.db.Patient;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
@@ -17,6 +25,11 @@ import androidx.recyclerview.widget.RecyclerView;
 
 public class AlertFragment extends Fragment {
     private AlertViewModel alertViewModel;
+
+    //데이터베이스에서 읽어올 정보들!
+    private Patient patient;
+
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup contatiner, Bundle savedInstanceState){
         alertViewModel =
@@ -25,5 +38,30 @@ public class AlertFragment extends Fragment {
 
 
         return root;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+        //확진자 수
+        DatabaseReference diagnosisRef = database.getReference("main");
+        diagnosisRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                patient = dataSnapshot.getValue(Patient.class);
+                Log.d("firebase - 확진자수 읽기", patient.getDiagnosis() + "");
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("firebase - 확진자수 읽기 에러", "Failed to read value.", error.toException());
+            }
+        });
     }
 }
