ff5279df601ab7805dc76fb67a6999d3a1c5300c
roseberr <bsc981111@naver.com>
2020-02-03 00:57:20 +0900

    rename

--- app/src/main/java/com/example/wuhan/MyFirebaseInstanceIdService.java
+++ app/src/main/java/com/example/wuhan/MyFirebaseInstanceIdService.java
@@ -1,4 +1,4 @@
 package com.example.wuhan;
 //: 디바이스에서 앱이 최초 실행되어 디바이스 토큰이 생성되거나 재생성 될 시 에 동작을 작성할 클래스
-public class FirebaseInstanceIdService {
+public class MyFirebaseInstanceIdService {
 }
--- app/src/main/java/com/example/wuhan/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/wuhan/MyFirebaseMessagingService.java
@@ -1,6 +1,6 @@
 package com.example.wuhan;
 //메시지를 수신하여 알림으로 보여주는 클래스
-public class FirebaseMessagingService {
+public class MyFirebaseMessagingService {
 
 
 
