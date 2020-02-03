954519d4ac89b4c2da04442bb9a1b04bdf6c6a77
roseberr <bsc981111@naver.com>
2020-02-03 20:50:36 +0900

    에러는 있지만 빨간줄없음

--- app/src/main/java/com/example/wuhan/MyFirebaseInstanceIdService.java
+++ app/src/main/java/com/example/wuhan/MyFirebaseInstanceIdService.java
@@ -4,10 +4,13 @@ package com.example.wuhan;
 import android.util.Log;
 
 import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingService;
+/*
 import com.google.firebase.iid.FirebaseInstanceIdService;
 
 public class MyFirebaseInstanceIdService extends FirebaseInstanceIdService {
-    @Override
+
     public void onTokenRefresh(){
         String refreshedToken = FirebaseInstanceId.getInstance().getToken();
         Log.d("IDService","Refreshed token : "+refreshedToken);
@@ -15,6 +18,18 @@ public class MyFirebaseInstanceIdService extends FirebaseInstanceIdService {
         sendRegistrationToServer(refreshedToken);
     }
 
-    private void sendRegistrationToServer(String token) {
+    private void sendRegistrationToServer(String token){
         //디바이스 토큰이 생성되거나 재생성 될 시 동작할 코드 작성
-    }}
+    }
+}*/
+
+
+public class MyFirebaseInstanceIdService extends FirebaseMessagingService {
+
+    String TAG="MyFirebaseInstanceIdService";
+
+    public void onNewToken(String s){
+        super.onNewToken(s);
+        Log.d("New_Token",s);
+    }
+}
--- app/src/main/java/com/example/wuhan/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/wuhan/MyFirebaseMessagingService.java
@@ -1,6 +1,7 @@
 package com.example.wuhan;
-//메시지를 수신하여 알림으로 보여주는 클래스
-//은노가 바꾼거
+
+
+
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -9,6 +10,7 @@ import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
+//import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import androidx.core.app.NotificationCompat;
@@ -16,62 +18,68 @@ import androidx.core.app.NotificationCompat;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import static androidx.constraintlayout.widget.Constraints.TAG;
+
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
-    private static final String TAG = "MsgService";
+   
+    public MyFirebaseMessagingService(){}
+
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
+        // ...
 
         // TODO(developer): Handle FCM messages here.
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
         Log.d(TAG, "From: " + remoteMessage.getFrom());
 
+        // Check if message contains a data payload.
         if (remoteMessage.getData().size() > 0) {
             Log.d(TAG, "Message data payload: " + remoteMessage.getData());
 
-            if (true) {
+            if (/* Check if data needs to be processed by long running job */ true) {
+                // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
+                scheduleJob();
             } else {
+                // Handle message within 10 seconds
                 handleNow();
             }
+
         }
 
+        // Check if message contains a notification payload.
         if (remoteMessage.getNotification() != null) {
-            Log.d(TAG, "Message Notification Title: " + remoteMessage.getNotification().getTitle());
             Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
-            sendNotification(remoteMessage.getNotification().getTitle(),remoteMessage.getNotification().getBody());
         }
+
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
     }
 
-    private void handleNow() {
-        Log.d(TAG, "Short lived task is done.");
+    private void scheduleJob() {
+    }
+    private void handleNow(){
+        
     }
 
-    private void sendNotification(String messageTitle, String messageBody) {
 
-        Intent intent = new Intent(this, MainActivity.class);
+    /**
+     * Called if InstanceID token is updated. This may occur if the security of
+     * the previous token had been compromised. Note that this is called when the InstanceID token
+     * is initially generated so this is where you would retrieve the token.
+     */
+    @Override
+    public void onNewToken(String token) {
+        Log.d(TAG, "Refreshed token: " + token);
 
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
-
-        String channelId = getString(R.string.default_notification_channel_id);
-        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-
-        NotificationCompat.Builder notificationBuilder =
-                new NotificationCompat.Builder(this, channelId)
-                        .setSmallIcon(R.mipmap.ic_launcher)
-                        .setContentTitle(messageTitle)
-                        .setContentText(messageBody)
-                        .setAutoCancel(true)
-                        .setSound(defaultSoundUri)
-                        .setContentIntent(pendingIntent);
-
-        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            String channelName = getString(R.string.default_notification_channel_name);
-            NotificationChannel channel = new NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH);
-            notificationManager.createNotificationChannel(channel);
-        }
-        notificationManager.notify(0, notificationBuilder.build());
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        sendRegistrationToServer(token);
     }
+
+    private void sendRegistrationToServer(String token) {
+    }
+
+
 }
