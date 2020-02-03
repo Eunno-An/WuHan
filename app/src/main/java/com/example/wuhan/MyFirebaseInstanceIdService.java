2560d090dc8e79da4915fca248ecadfa329d0f06
roseberr <bsc981111@naver.com>
2020-02-03 19:17:51 +0900

    push알림
    아직오류남

--- app/src/main/java/com/example/wuhan/MyFirebaseInstanceIdService.java
+++ app/src/main/java/com/example/wuhan/MyFirebaseInstanceIdService.java
@@ -1,4 +1,20 @@
 package com.example.wuhan;
-//: 디바이스에서 앱이 최초 실행되어 디바이스 토큰이 생성되거나 재생성 될 시 에 동작을 작성할 클래스
-public class MyFirebaseInstanceIdService {
-}
+//디바이스에서 앱이 최초 실행되어 디바이스
+// 토큰이 생성되거나 재생성 될 시 에 동작을 작성할 클래스
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class MyFirebaseInstanceIdService extends FirebaseInstanceIdService {
+    @Override
+    public void onTokenRefresh(){
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d("IDService","Refreshed token : "+refreshedToken);
+
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    private void sendRegistrationToServer(String token) {
+        //디바이스 토큰이 생성되거나 재생성 될 시 동작할 코드 작성
+    }}
